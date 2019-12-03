fc0fee936f8497400dc0fdca444c4ff2e7599a46
houchangliang123 <1057322345@qq.com>
2019-12-03 22:17:30 +0800

    12/3

--- src/JDKDemo.java
+++ src/JDKDemo.java
@@ -1,5 +1,20 @@
+import java.util.function.Function;
+
 public class JDKDemo {
+
+    public void aaa(Function<Object,String> interface1) {
+        System.out.println(interface1.apply("afhbhnh"));
+    }
     public static void main(String[] args) {
         System.out.println("ssss");
+        JDKDemo jdkDemo = new JDKDemo();
+        jdkDemo.aaa(a -> a+"a");
+    }
+}
+interface Interface1<T>{
+    public abstract T a(String a);
+
+    public default int b(int x){
+        return x+1;
     }
 }
